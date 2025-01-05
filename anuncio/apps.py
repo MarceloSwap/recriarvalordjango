b5fc6e1b19ad6dad2c74a1df976a9fdeffcf8570
MarceloSwap <126257691+MarceloSwap@users.noreply.github.com>
2025-01-05 15:37:25 -0300

    update superuser

--- anuncio/apps.py
+++ anuncio/apps.py
@@ -16,6 +16,5 @@ class AnuncioConfig(AppConfig):
         # Pesquisar se tem o usuario com o email acima
         #Usuario tá sendo puchado do banco
         usuarios = Usuario.objects.filter(email=email)
-        #SE a lista tá vazia
         if not usuarios:
-            Usuario.objects.create_superuser(username="Marcelo", emai=email, password=senha, is_active=True, is_staff=True)
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
